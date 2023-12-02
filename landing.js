299ed1e5139b8e381c077547d7c8dce346ce4ba1
LenilsonErnesto <137925767+LenilsonErnesto@users.noreply.github.com>
2023-12-01 23:05:39 -0300

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,8 @@
+
+const btn= document.querySelector('button').addEventListener(
+  "click", () =>{
+    const link = "./Seach_page/search.html";
+    if(true){
+      window.location.href= link;
+    }
+  });
