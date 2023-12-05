070d3ff9549a171d7d8ab20ec1d9260f5f6a8136
LenilsonErnesto <137925767+LenilsonErnesto@users.noreply.github.com>
2023-12-04 22:50:59 -0300

    Delete landing page directory

--- landing page/landing.js
+++
@@ -1,8 +0,0 @@
-
-const btn= document.querySelector('button').addEventListener(
-  "click", () =>{
-    const link = "./Seach_page/search.html";
-    if(true){
-      window.location.href= link;
-    }
-  });
