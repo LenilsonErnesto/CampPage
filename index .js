67928a1d736a9898225f4a786c418b348e0779f8
LenilsonErnesto <137925767+LenilsonErnesto@users.noreply.github.com>
2023-12-04 22:53:47 -0300

    Update index .js

--- index .js
+++ index .js
@@ -4,7 +4,7 @@ window.addEventListener("click",
 const header= document.querySelector('header');
   header.innerHTML= 
   `<div class="header-svg">
-    <img src="assets/Logo.svg"/>
+    <img src="./assets/Logo.svg"/>
   </div>
   <div class="headerMenu-icon">
     <i class="ph ph-list"></i>
